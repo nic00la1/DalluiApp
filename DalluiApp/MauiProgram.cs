5d9eedfdb3b4d9c102aa244e29f4b729229104c8
nic00la1 <nic00la.kaleta@gmail.com>
2023-11-07 10:05:46 +0100

    Applied images added by AI

--- DalluiApp/MauiProgram.cs
+++ DalluiApp/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using PanCardView;
 
 namespace DalluiApp
 {
@@ -9,6 +10,7 @@ namespace DalluiApp
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseCardsView()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
