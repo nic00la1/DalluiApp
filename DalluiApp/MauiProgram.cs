645ada062d8954ba6cdafa283eec4a589e7397c3
nic00la1 <nic00la.kaleta@gmail.com>
2023-11-08 09:58:07 +0100

    Added label to a AI generated images

--- DalluiApp/MauiProgram.cs
+++ DalluiApp/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 using PanCardView;
 
 namespace DalluiApp
@@ -11,6 +12,7 @@ namespace DalluiApp
             builder
                 .UseMauiApp<App>()
                 .UseCardsView()
+                .UseMauiCommunityToolkit()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
