3c2192f441a8625646158ea90a1b8abbe61294fd
nic00la1 <nic00la.kaleta@gmail.com>
2023-11-11 22:16:11 +0100

    add a begginer animation

--- DalluiApp/Views/ImageGeneratorView.xaml.cs
+++ DalluiApp/Views/ImageGeneratorView.xaml.cs
@@ -12,11 +12,17 @@ public partial class ImageGeneratorView : ContentPage
 		StopGeneration();
     }
 
-	private void StopGeneration()
+	private async void StopGeneration()
 	{
 		lottie.IsAnimationEnabled = false;
 		lottie.IsVisible = false;
-
 		imageBorder.IsVisible = true;
+
+		await Task.WhenAny(
+			imageAnimation.ScaleTo(1.1, 1000),
+			imageAnimation.FadeTo(0, 1000),
+            borderTime.ScaleTo(1, 1000),
+            borderTime.FadeTo(1, 1000)
+            );
 	}
 }
